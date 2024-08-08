611307fa2fc294b3936965111ed2750377b02af4
Harshita Mahajan <harshitamahajan2020@gmail.com>
2024-08-08 10:51:05 +0530

    ProductDisplay

--- src/App.js
+++ src/App.js
@@ -26,8 +26,9 @@ function App() {
         <Route path='/men' element={<ShopCategory banner={men_banner} category="men"/>}/>
         <Route path='/women' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
-        <Route path="/product" element={<Product/>}/>
-        <Route path=":productId" element={<Product/>}></Route>
+        <Route path="/product" element={<Product/>}>
+         <Route path=":productId" element={<Product/>}/>
+         </Route>
         <Route path="/cart" element={<Cart/>}></Route>
         <Route path="/login" element={<LoginSignup/>}></Route>
 
--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -4,11 +4,20 @@ import arrow_icon from "../Assets/arrow.png"
 
 const Breadcrum = (props) => {
   const {product}=props;
-  return (
-    <div className='breadcrum'>
-      HOME <img src={arrow_icon} alt="" /> Shop <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+
+  const {category,name}=product;
+  
+  
+  return (  
+    
+     <div className='breadcrum'>
+
+
+       
+      HOME <img src={arrow_icon} alt="" /> Shop <img src={arrow_icon} alt="" /> {category} <img src={arrow_icon} alt="" /> {name}
     </div>
-  )
+)
+  
 }
 
 export default Breadcrum
